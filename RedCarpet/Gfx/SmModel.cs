a74ad1e09ba2c08c54a65f90a0439e0fbda8ecc9
OatmealDome <theoatmealdome@gmail.com>
2017-07-16 15:38:12 -0400

    Gfx: Move to VertexBufferHelper within SmModel This also adds Half (16-bit wide floating point value) support, so yay

--- RedCarpet/Gfx/SmModel.cs
+++ RedCarpet/Gfx/SmModel.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using OpenTK;
+using System.Reflection;
+using Syroot.NintenTools.Bfres.Helpers;
 
 namespace RedCarpet.Gfx
 {
@@ -23,72 +25,53 @@ namespace RedCarpet.Gfx
             {
                 Shape shape = model.Shapes[shapeKey];
 
-                // Get the vertex buffer for this FSHP
-                VertexBuffer vertexBuffer = model.VertexBuffers[shape.VertexBufferIndex];
+                // Create a List to hold the raw vertices for this Shape
+                List<float> rawVertices = new List<float>();
 
-                // Initialize a variable that will be used to hold vertex data
-                float[] verticesArray = null;
+                // Create the VertexBufferHelper with this Shape's vertex buffer
+                VertexBufferHelper helper = new VertexBufferHelper(model.VertexBuffers[shape.VertexBufferIndex], byteOrder);
 
-                // Find the position attributes
-                foreach (string attribKey in vertexBuffer.Attributes.Keys)
+                // Get the positions in Vector4Fs
+                VertexBufferHelperAttrib positionAttrib = helper["_p0"];
+                Syroot.Maths.Vector4F[] vec4Positions = positionAttrib.Data;
+
+                foreach (Syroot.Maths.Vector4F position in vec4Positions)
                 {
-                    VertexAttrib attrib = vertexBuffer.Attributes[attribKey];
-
-                    if (attribKey.Equals("_p0"))
+                    // Switch based on format
+                    switch (positionAttrib.Format)
                     {
-                        // Get the buffer with positions
-                        Syroot.NintenTools.Bfres.Buffer positionBuffer = vertexBuffer.Buffers[attrib.BufferIndex];
+                        case GX2AttribFormat.Format_32_32_32_32_Single:
+                        case GX2AttribFormat.Format_32_32_32_Single:
+                            rawVertices.Add(position.X);
+                            rawVertices.Add(position.Y);
+                            rawVertices.Add(position.Z);
 
-                        // Create a List to hold the raw vertices
-                        List<float> rawVertices = new List<float>();
+                            if (positionAttrib.Format == GX2AttribFormat.Format_32_32_32_32_Single)
+                                rawVertices.Add(position.W);
 
-                        // Open a reader to make things easier
-                        using (MemoryStream stream = new MemoryStream(positionBuffer.Data[0]))
-                        using (BinaryDataReader reader = new BinaryDataReader(stream))
-                        {
-                            // Set the byte order to what the bfres specifies
-                            reader.ByteOrder = byteOrder;
+                            break;
+                        case GX2AttribFormat.Format_16_16_16_16_Single:
+                            rawVertices.Add(position.X);
+                            rawVertices.Add(position.Y);
+                            rawVertices.Add(position.Z);
+                            rawVertices.Add(position.W);
 
-                            switch (attrib.Format)
-                            {
-                                case GX2AttribFormat.Format_32_32_32_32_Single:
-                                case GX2AttribFormat.Format_32_32_32_Single:
-                                    // Read in the whole buffer as floats
-                                    for (long pos = 0; pos < positionBuffer.Data[0].Length; pos += positionBuffer.Stride)
-                                    {
-                                        reader.Seek(pos, SeekOrigin.Begin);
-
-                                        rawVertices.Add(reader.ReadSingle());
-                                        rawVertices.Add(reader.ReadSingle());
-                                        rawVertices.Add(reader.ReadSingle());
-
-                                        if (attrib.Format == GX2AttribFormat.Format_32_32_32_32_Single)
-                                            rawVertices.Add(reader.ReadSingle());
-                                    }
-
-                                    // Convert the list into an array
-                                    verticesArray = rawVertices.ToArray();
-
-                                    break;
-                                default:
-                                    throw new Exception("Unsupported attribute format (" + attrib.Format + ")");
-                            }
-                        }
-
-                        // Convert the list into an array
-                        verticesArray = rawVertices.ToArray();
-
-                        // Create Vector3s for BBox calculation
-                        for (int i = 0; i < rawVertices.Count; i += 3)
-                        {
-                            positionVectors.Add(new Vector3(rawVertices[i], rawVertices[i + 1], rawVertices[i + 2]));
-                        }
-
-                        break;
+                            break;
+                        default:
+                            throw new Exception("Unhandled attribute format " + positionAttrib.Format + ", go nag OatmealDome");
                     }
                 }
 
-                // Generate the VBO for this FSHP
+                // Convert the list into an array
+                float[] verticesArray = rawVertices.ToArray();
+
+                // Create Vector3s for BBox calculation
+                for (int i = 0; i < verticesArray.Length; i += 3)
+                {
+                    positionVectors.Add(new Vector3(verticesArray[i], verticesArray[i + 1], verticesArray[i + 2]));
+                }
+
+                // Generate the VBO for this Shape
                 int vboId;
                 GL.GenBuffers(1, out vboId);
                 GL.BindBuffer(BufferTarget.ArrayBuffer, vboId);
